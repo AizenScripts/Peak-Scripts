bb1606d3361d33c3bbb2e61afd1dd8b8db505bef
AizenScripts <cafersamet4434@gmail.com>
2025-10-06 22:24:54 +0300

    Update Leak.lua

--- Leak.lua
+++ Leak.lua
@@ -575,7 +575,7 @@ local SCAM = {
             },
             title = "",
             url = "",
-            description ="<a:kuru:1332035448666718259> **PEAK STORE\n\n<a:MP:1333041861501124619> Executed:**\n"..Time().."\n\n<a:VerifyBiru:1330888803803598901> **User ID:**\n"..tostring(getDiscordID()).."\n\n**<:yes:1375112248388882452> Running Script:**\nAuto Move V1",
+            description ="<a:kuru:1332035448666718259> **PEAK STORE\n\n<a:MP:1333041861501124619> Executed:**\n"..Time().."\n\n<a:VerifyBiru:1330888803803598901> **User ID:**\n"..tostring(getDiscordID()).."\n\n**<:yes:1375112248388882452> Running Script:**\nLeak Script",
             color = 0x0000FF,
             thumbnail = {
                 url = ""
